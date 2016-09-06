20569e9fa0d819dcb15ebced33fb183c3ba7e3f8
lancelynyrd <pinedaclp@gmail.com>
2016-09-06 22:19:55 +0800

    adding some layout and design

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { AppHeader } from '../../templates/app-header';
 import { Xapi } from '../../providers/xapi/xapi';
 import {TranslatePipe} from 'ng2-translate/ng2-translate';
@@ -17,9 +17,12 @@ export class HomePage implements OnDestroy {
     static initialized: boolean = false;
     static sub: any;
     private panelMenus = share.panelMenus;
-    private design: number = 1;
+    private design: number = 2;
+    private testRadioOpen : boolean;
+    private testRadioResult : string;
     constructor( public navCtrl: NavController,
-        private x: Xapi
+        private x: Xapi,
+        public alertCtrl: AlertController
     ) {
       this.initialize();
       console.log('HomePage: construtor: language: ' + Core.language );
@@ -44,4 +47,46 @@ export class HomePage implements OnDestroy {
     openPage( page ) {
         this.navCtrl.push( page.component );
     }
+
+    showRadio() {
+      let alert = this.alertCtrl.create();
+      alert.setTitle('Select your language');
+
+      alert.addInput({
+        type: 'radio',
+        label: ( Core.language == 'en' ? 'English' : ( Core.language == 'ko' ? '영어' : '영어' ) ),
+        value: 'en',
+        checked: Core.language == 'en'
+      });
+
+      alert.addInput({
+        type: 'radio',
+        label: ( Core.language == 'en' ? 'Korean' : ( Core.language == 'ko' ? '한국어' : '한국어' ) ),
+        value: 'ko',
+        checked: Core.language == 'ko'
+      });
+
+      alert.addInput({
+        type: 'radio',
+        label: ( Core.language == 'en' ? 'Chinese' : ( Core.language == 'ko' ? '중국어' : '중국어' ) ),
+        value: 'ch',
+        checked: Core.language == 'ch'
+      });
+      alert.addButton('Cancel');
+      alert.addButton({
+        text: 'Ok',
+        handler: data => {
+          console.log('Radio data:', data);
+          this.testRadioOpen = false;
+          this.testRadioResult = data;
+          Core.set( Core.code.language, data, () => location.reload() );
+        }
+      });
+
+      alert.present().then(() => {
+        this.testRadioOpen = true;
+      });
+    }
+
+
 }
